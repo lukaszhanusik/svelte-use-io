0a49628367a760b91b43d78ce95874f3edb66cb7
Lukasz Hanusik <lukasz.hanusik@trustly.com>
2022-04-10 17:29:34 +0200

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -70,7 +70,7 @@
 			<div>
 				<code>npm i svelte-use-io</code>
 				·
-				<span>Complaint box: <a href="https://twitter.com/DerekNguyen10">@analphabeta</a></span>
+				<span>Complaint box: <a href="https://twitter.com/0000000000000000">@analphabeta</a></span>
 			</div>
 		</div>
 		<nav class="main-nav" data-grid-name="b">
